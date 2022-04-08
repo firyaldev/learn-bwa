2a7d6c0eaec0fc1b86af2cf1809a819bf6948eaf
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-08 16:30:43 +0700

    add: todo 4 Component Migration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrived from './components/Arrived';
+import Client from './components/Client';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Hello perople with spirit of learning</h1>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Client />
+      <AsideMenu />
+      <Footer />
     </div>
   );
 }
